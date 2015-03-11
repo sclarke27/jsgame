6c0a4b621eca370c1b39e85381ee2b384e59d99f
sclarke27 <sclarke27@gmail.com>
2015-03-11 15:45:51 -0700

    fixes for platform game

---
+++ lawn_maps/basic 4 square.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="basic 4 square" tilewidth="20" tileheight="20">
+ <image source="basic4square.png" trans="ffffff" width="40" height="40"/>
+ <tile id="0">
+  <properties>
+   <property name="coords" value="0,0"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="coords" value="0,1"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="coords" value="1,0"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="coords" value="1,1"/>
+  </properties>
+ </tile>
+</tileset>
---
+++ lawn_maps/whites.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="whites" tilewidth="20" tileheight="20">
+ <image source="basic-4-square-white.png" trans="00ff00" width="40" height="40"/>
+ <tile id="0">
+  <properties>
+   <property name="coords" value="0,0"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="coords" value="0,1"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="coords" value="1,0"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="coords" value="1,1"/>
+  </properties>
+ </tile>
+</tileset>
